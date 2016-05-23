5a8a9274f4613c271db840414b33cbfb0a3aae9c
Brandon Greenwell <greenwell.brandon@gmail.com>
2016-05-23 09:28:38 -0400

    doc update

--- R/plotPartial.R
+++ R/plotPartial.R
@@ -1,3 +1,13 @@
+#' Plotting Partial Dependence Functions
+#'
+#' Plots 1- and 2-dimensional partial dependence functions.
+#'
+#' @param x An object of class{"partial_1d"} or \code{"partial_2d"}.
+#' @param contour Logical indicating whether or not to use \code{lattice::levelplot} 
+#'   (\code{TRUE}) or \code{lattice::wireframe} (\code{FALSE}). Default is \code{TRUE}.
+#' @param ... Additional optional arguments to be passed onto \code{lattice}
+#'   plotting functions.
+#' @export
 plotPartial <- function(x, ...) {
   UseMethod("plotPartial")
 }

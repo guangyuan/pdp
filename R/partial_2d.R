384a8dcf292bd215548ff09482289d4179702409
Brandon Greenwell <greenwell.brandon@gmail.com>
2016-05-24 08:14:55 -0400

    updated code

--- R/partial_2d.R
+++ R/partial_2d.R
@@ -23,46 +23,89 @@
 #' (with \code{unbiased = TRUE}).
 #' @importFrom plyr adply
 #' @export
-partial_2d <- function(object, x1.name, x2.name, n1, n2, x1.class, x2.class,
-                       newdata, ...) {
+partial_2d <- function(object, ...) {
+  UseMethod("partial_1d")
+}
 
+
+partial_2d.default <- function(object, x1.name, x2.name, n1, n2, 
+                               x1.values, x2.values, x1.class, x2.class, 
+                               newdata, ...) {
+  
   # Data frame
-  .data <- if (missing(newdata)) eval(object$call$data) else newdata
-
-  # Sorted unique values of the first independent variable
-  sux1 <- sort(unique(.data[[x1.name]]))
-  if (!missing(n1)) {
-    sux1 <- seq(from = min(sux1), to = max(sux1), length = n1)
+  newdata <- if (missing(newdata)) eval(object$call$data) else newdata
+  
+  x1.class <- class(newdata[[x1.name]])
+  x2.class <- class(newdata[[x2.name]])
+  
+  # First predictor values of interest
+  x1.values <- if (missing(x1.values)) {
+    if (x1.class == "factor") {
+      levels(newdata[[x1.name]])
+    } else if (missing(n1)) {
+      sort(unique(newdata[[x1.name]]))
+    } else {
+      seq(from = min(newdata[[x1.name]]), 
+          to = max(newdata[[x1.name]]), length = n1)
+    }
   }
-
-  # Sorted unique values of the second independent variable
-  sux2 <- sort(unique(.data[[x2.name]]))
-  if (!missing(n2)) {
-    sux2 <- seq(from = min(sux2), to = max(sux2), length = n2)
+  
+  # Second predictor values of interest
+  x2.values <- if (missing(x2.values)) {
+    if (x1.class == "factor") {
+      levels(newdata[[x2.name]])
+    } else if (missing(n2)) {
+      sort(unique(newdata[[x2.name]]))
+    } else {
+      seq(from = min(newdata[[x2.name]]), 
+          to = max(newdata[[x2.name]]), length = n2)
+    }
   }
-
+  
   # Data frame of unique combinations
-  xgrid <- expand.grid("x1" = sux1, "x2" = sux2)
-
-  # Make sure x1 and x2 have the specified class
+  xgrid <- expand.grid("x1" = x1.values, "x2" = x2.values)
+  
+  # Determine the type of supervised learning used
+  if (missing(super.type)) {
+    super.type <- superType(object)
+  } else {
+    if (!(super.type %in% c("regression", "classification"))) {
+      stop("Only regression and classification are supported.")
+    }
+  }
+  
+  # Make sure x1 and x2 have the correct class
   if (!missing(x1.class)) {
     class(xgrid$x1) <- x1.class
   }
   if (!missing(x2.class)) {
     class(xgrid$x2) <- x2.class
   }
-
-  # Compute average prediction for each unique value
-  pd_df <- adply(xgrid, .margins = 1, .fun = function(x) {
-    temp <- .data
-    temp[[x1.name]] <- x[[1L]]
-    temp[[x2.name]] <- x[[2L]]
-    mean(predict(object, newdata = temp), na.rm = TRUE)
-  }, ...)
-
+  
+  # Calculate partial dependence values
+  if (super.type == "regression") {
+    pd_df <- adply(xgrid, .margins = 1, .fun = function(x) {
+      temp <- newdata
+      temp[[x1.name]] <- x[[1L]]
+      temp[[x2.name]] <- x[[2L]]
+      mean(predict(object, newdata = temp), na.rm = TRUE)
+    }, ...)
+  } else if (super.type == "classification") {
+    pd_df <- adply(xgrid, .margins = 1, .fun = function(x) {
+      temp <- newdata
+      temp[[x1.name]] <- x[[1L]]
+      temp[[x2.name]] <- x[[2L]]
+      pr <- predict(object, newdata = temp, type = "prob")
+      avgLogit(pr, which.class = which.class)
+    }, ...)
+  } else {
+    stop(paste("Partial dependence values are currently only available",
+               "for classification and regression problems."))
+  }
+  
   # Return data frame of partial dependence values
   names(pd_df) <- c(x1.name, x2.name, "y")
   class(pd_df) <- c("data.frame", "partial_2d")
   pd_df
-
+  
 }

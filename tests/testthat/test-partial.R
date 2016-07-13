63ea0b4c39bfb66351942b7521c686dadcd458f6
Brandon Greenwell <greenwell.brandon@gmail.com>
2016-07-13 14:15:36 -0400

    updated partial tests

--- tests/testthat/test-partial.R
+++ tests/testthat/test-partial.R
@@ -5,11 +5,23 @@ context("Partial dependence functions")
 #   classification: iris
 
 test_that("partial works correctly", {
+
+  # Regression
   mtcars.lm <- lm(mpg ~ ., data = mtcars)
   pd1 <- partial(mtcars.lm, pred.var = "wt", grid.resolution = 1, 
-               super.type = "regression", training.data = mtcars)
+                 super.type = "regression", training.data = mtcars)
   pd2 <- partial(mtcars.lm, pred.var = "wt", pred.grid = 1)
   expect_is(pd1, "data.frame")
   expect_is(pd2, "data.frame")
   expect_is(partial(mtcars.lm, pred.var = "wt", plot = TRUE), "trellis")
+  
+  # Classification
+  if (require(randomForest, quietly = TRUE)) {
+    iris.rf <- randomForest(Species ~ ., data = iris)
+    iris.un <- randomForest( ~ ., data = iris)
+    pd <- partial(iris.rf, pred.var = "Petal.Width", grid.resolution = 1)
+    expect_is(pd, "data.frame")
+    expect_error(partial(iris.un, pred.var = "Petal.Width", grid.resolution = 1))
+  }
+  
 })

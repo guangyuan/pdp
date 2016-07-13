779fc965d78d7633fe78329161706eb6f531c35f
Brandon Greenwell <greenwell.brandon@gmail.com>
2016-07-13 11:07:58 -0400

    added first couple of tests!

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,7 +1,24 @@
 context("Utility functions")
 
+# Data frames used in the following tests
+#   regression: mtcars
+#   classification: iris
+
+test_that("avgLogit works correctly", {
+  pm <- matrix(c(0.1, 0.3, 0.6), nrow = 1, ncol = 2, byrow = TRUE)
+  expect_error(avgLogit(as.data.frame(pm)))
+  expect_identical(avgLogit(pm, which.class = 1L), 
+                   log(0.1) - (log(0.1) + log(0.3) + log(0.6)) / 3)
+  expect_identical(avgLogit(pm, which.class = 2L), 
+                   log(0.3) - (log(0.1) + log(0.3) + log(0.6)) / 3)
+  expect_identical(avgLogit(pm, which.class = 3L), 
+                   log(0.6) - (log(0.1) + log(0.3) + log(0.6)) / 3)
+})
+
 test_that("superType works correctly", {
 
-  # Expectations
+  # Linear models
+  mtcars.lm <- lm(mpg ~ ., data = mtcars)
+  expect_identical(superType(fit.lm), "regression")
 
 })

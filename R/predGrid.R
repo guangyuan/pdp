ac88560109bc37725e55dfe914c517633b19790b
Brandon Greenwell <greenwell.brandon@gmail.com>
2017-01-10 11:44:21 -0500

    added quantiles and probs arguments

--- R/predGrid.R
+++ R/predGrid.R
@@ -12,17 +12,25 @@ predGrid <- function(object, pred.var, train, grid.resolution = NULL) {
 
 
 #' @keywords internal
-predGrid.default <- function(object, pred.var, train, grid.resolution = NULL) {
+predGrid.default <- function(object, pred.var, train, grid.resolution = NULL,
+                             quantiles = FALSE, probs = 1:9/10) {
   pred.val <- lapply(pred.var, function(x) {
     if (is.factor(train[[x]])) {
       levels(train[[x]])
     } else {
-      if (is.null(grid.resolution)) {
-        grid.resolution <- min(length(unique(train[[x]])), 51)
+      if (!is.null(grid.resolution) && quantiles) {
+        stop("Can only specify one of grid.resolution or quantiles, not both.")
       }
-      seq(from = min(train[[x]], na.rm = TRUE),
-          to = max(train[[x]], na.rm = TRUE),
-          length = grid.resolution)
+      if (quantiles) {
+        quantile(train[[x]], probs = probs, na.rm = TRUE, names = FALSE)
+      } else {
+        if (is.null(grid.resolution)) {
+          grid.resolution <- min(length(unique(train[[x]])), 51)
+        }
+        seq(from = min(train[[x]], na.rm = TRUE),
+            to = max(train[[x]], na.rm = TRUE),
+            length = grid.resolution)
+      }
     }
   })
   pred.grid <- expand.grid(pred.val)

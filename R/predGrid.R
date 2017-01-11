21a889638f24ebf55b08246528e0a2effb8d7c2f
Brandon Greenwell <greenwell.brandon@gmail.com>
2017-01-11 10:45:35 -0500

    new argument trim.outliers

--- R/predGrid.R
+++ R/predGrid.R
@@ -6,15 +6,24 @@
 # predGrid.RandomForest <- NULL
 
 #' @keywords internal
+trimOutliers <- function(x) {
+  out <- boxplot.stats(x, do.out = TRUE)$out
+  x[!(x %in% out)]  # faster than setdiff in benchmark test
+}
+
+
+#' @keywords internal
 predGrid <- function(object, pred.var, train, grid.resolution = NULL,
-                     quantiles = FALSE, probs = 1:9/10) {
+                     quantiles = FALSE, probs = 1:9/10,
+                     trim.outliers = FALSE) {
   UseMethod("predGrid")
 }
 
 
 #' @keywords internal
 predGrid.default <- function(object, pred.var, train, grid.resolution = NULL,
-                             quantiles = FALSE, probs = 1:9/10) {
+                             quantiles = FALSE, probs = 1:9/10,
+                             trim.outliers = FALSE) {
   pred.val <- lapply(pred.var, function(x) {
     if (is.factor(train[[x]])) {
       levels(train[[x]])
@@ -22,14 +31,21 @@ predGrid.default <- function(object, pred.var, train, grid.resolution = NULL,
       if (!is.null(grid.resolution) && quantiles) {
         stop("Can only specify one of grid.resolution or quantiles, not both.")
       }
+      if (quantiles && trim.outliers) {
+        stop("Can only specify one of quantiles or trim.outliers, not both.")
+      }
       if (quantiles) {
         quantile(train[[x]], probs = probs, na.rm = TRUE, names = FALSE)
       } else {
         if (is.null(grid.resolution)) {
           grid.resolution <- min(length(unique(train[[x]])), 51)
         }
-        seq(from = min(train[[x]], na.rm = TRUE),
-            to = max(train[[x]], na.rm = TRUE),
+        y <- if (trim.outliers) {
+          trimOutliers(train[[x]])
+        } else {
+          train[[x]]
+        }
+        seq(from = min(y, na.rm = TRUE), to = max(y, na.rm = TRUE),
             length = grid.resolution)
       }
     }
@@ -38,3 +54,4 @@ predGrid.default <- function(object, pred.var, train, grid.resolution = NULL,
   names(pred.grid) <- pred.var
   pred.grid
 }
+

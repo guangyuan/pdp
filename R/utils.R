041d362fa89f04b408b8d1859a0ff819e2f9648c
Brandon Greenwell <greenwell.brandon@gmail.com>
2017-06-05 11:00:55 -0400

    rm commented out dplyr code

--- R/utils.R
+++ R/utils.R
@@ -68,12 +68,6 @@ centerIceCurves <- function(object) {
 
 #' @keywords internal
 centerIceCurves.ice <- function(object) {
-  # res <- object %>%
-  #   dplyr::group_by_("yhat.id") %>%
-  #   dplyr::mutate_("yhat" = "yhat - first(yhat)") %>%
-  #   dplyr::ungroup()
-  # class(res) <- c("data.frame", "cice")
-  # res
   yhat <- tapply(object[["yhat"]], INDEX = as.factor(object[["yhat.id"]]),
                  FUN = function(x) x - x[1L], simplify = FALSE)
   res <- data.frame("x" = object[[1L]],
